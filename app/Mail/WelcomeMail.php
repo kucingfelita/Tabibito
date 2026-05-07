69aa1ad0d07f6e9ce8a21225489a9c874f29ce1b
ethan <hariansyahegie@gmail.com>
2026-05-07 10:08:04 +0700

    ubah nama

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -27,7 +27,7 @@ class WelcomeMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Selamat datang di Marketplace Wisata',
+            subject: 'Selamat datang di Tabibito Jateng',
         );
     }
 
